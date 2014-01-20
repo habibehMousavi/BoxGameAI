ba0f99e9127ee865108a835b8ce33dbaac682040
habibehMousavi <habibeh_mousavi71@yahoo.com>
2014-01-21 00:12:06 +0330

    correctness in

---
+++ 1.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        public int turn = 0;
+        public string message = "";
+        public int[] move = new int[4];
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.manageServerMessage("Start()");
+            // p.manageServerMessage("Recived()");
+            //p.manageServerMessage("[1,1,1,1]");???
+           // p.manageServerMessage("Time`sUP()|[1,1,1,1]");
+            // p.manageServerMessage("InValid()|[1,1,1,1]");
+             p.manageServerMessage("Bonus(+2,A)|[1,1,1,1]");
+             p.manageServerMessage("Bonus(+1,A)|[1,1,1,1]");
+             p.manageServerMessage("Bonus(+2,A)");
+             p.manageServerMessage("Bonus(+1,A)");
+            // manageServerMessage('Time`sUP()|[1,1,1,1]Bonus(+1,A)');
+            // manageServerMessage('InValid()|[1,1,1,1]Bonus(+1,A)');
+        }
+
+        public void manageServerMessage(string text)
+        {
+
+           // if (text.Substring(0, 1) == "S")
+            //{
+              //  turn++;
+           // }
+
+
+           // string regexBracket = @"\[([0-9]),([0-9]|1[0-4]),([0-9]),([0-9]|1[0-4])]";
+
+            //Regex r = new Regex(regexBracket, RegexOptions.IgnoreCase);
+
+           // Match m = r.Match(text);
+
+            //if (m.Success)
+            //{
+             //   move[0] = Convert.ToInt16(m.Groups[1]);
+             //   move[1] = Convert.ToInt16(m.Groups[2]);
+            //    move[2] = Convert.ToInt16(m.Groups[3]);
+              //  move[3] = Convert.ToInt16(m.Groups[4]);
+           // }
+
+
+
+
+            
+            
+           // else if (text.Substring(0, 1) == "R")
+            //{
+            //  turn--;
+            //}
+
+           //  string regexBracket = @"\[([0-9]),([0-9]|1[0-4]),([0-9]),([0-9]|1[0-4])]";
+
+           // Regex r = new Regex(regexBracket, RegexOptions.IgnoreCase);
+
+           // Match m = r.Match(text);
+
+           // if (m.Success)
+           // {
+           //     move[0] = Convert.ToInt16(m.Groups[1]);
+           //     move[1] = Convert.ToInt16(m.Groups[2]);
+            //    move[2] = Convert.ToInt16(m.Groups[3]);
+            //    move[3] = Convert.ToInt16(m.Groups[4]);
+          //  }
+
+
+
+
+           // if (text.Substring(0, 1) == "T" )
+           //{
+           //     turn--;
+           // }
+
+           // string regexBracket = @"\(Time`sUP\(\)|InValid\(\))\|(\[([0-9]),([0-9]|1[0-4]),([0-9]),([0-9]|1[0-4])])";
+
+           // Regex r = new Regex(regexBracket, RegexOptions.IgnoreCase);
+
+           // Match m = r.Match(text);
+
+           // if (m.Success)
+           // {
+           //     message = Convert.ToString(m.Groups[1]);
+
+           //     move[0] = Convert.ToInt16(m.Groups[3]);
+           //     move[1] = Convert.ToInt16(m.Groups[4]);
+           //     move[2] = Convert.ToInt16(m.Groups[5]);
+           //     move[3] = Convert.ToInt16(m.Groups[6]);
+           // }
+
+
+
+            if(text.Substring(0,1)=="B")
+            {
+               
+            string regexBracket = @"\  Bonus\(\+([1-2]),[A-B]\)(\|\[([0-9]),([0-9]|1[0-4]),([0-9]),([0-9]|1[0-4])])";
+
+            Regex r = new Regex(regexBracket, RegexOptions.IgnoreCase);
+
+            Match m = r.Match(text);
+
+            if (m.Success)
+
+            {
+                int count;
+
+                 count= Convert.toInt16(m.Groups[1]);
+
+                if( m.Groups[3]==null)
+                    turn=count;
+                else 
+                    turn=-count+1;
+
+
+                move[0] = Convert.ToInt16(m.Groups[3]);
+                move[1] = Convert.ToInt16(m.Groups[4]);
+                move[2] = Convert.ToInt16(m.Groups[5]);
+                move[3] = Convert.ToInt16(m.Groups[6]);
+            }
+
+          
+
+            Console.WriteLine("turn:");
+            Console.WriteLine(turn.ToString());
+            Console.WriteLine("");
+
+            Console.WriteLine("move:");
+            Console.WriteLine(string.Join(",", move));
+            Console.WriteLine("");
+
+            Console.WriteLine("message:");
+            Console.WriteLine(message);
+            Console.WriteLine("");
+
+        }
+    }
+}
+
+
+
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace SocketClient_2
+namespace WindowsFormsApplication2
 {
     static class Program
     {
